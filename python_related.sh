fe331c5f0f12ab099f29c1b582d1215dfe8d96dc
Carleux <carloscacheux@outlook.com>
2026-03-18 16:59:54 -0400

    get python version numbers

---
+++ python_related.sh
@@ -0,0 +1,26 @@
+function get_python_major_version {
+
+    org_ifs=$IFS
+    local pyv="$1"
+    IFS='.' read -ra version_numbers <<< "$pyv"
+    
+    for version in "${version_numbers[@]}"; do
+        pytnon_version+=($version)
+    done
+      
+   python_major_version="${pytnon_version[0]}"
+   echo $python_major_version
+}
+
+function get_python_package_version {
+
+    org_ifs=$IFS
+    IFS='.' read -ra version_numbers <<< "$pyv"
+    
+    for version in "${version_numbers[@]}"; do
+        pytnon_version+=($version)
+    done
+   
+   package_version="${pytnon_version[0]}.${pytnon_version[1]}"
+   echo $package_version
+}
